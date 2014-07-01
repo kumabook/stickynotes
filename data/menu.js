ab6270c77b66f3ceed8858666f5a9b3d62da6246
kumabook <kumabook@live.jp>
2014-07-02 01:09:56 +0900

    [#20] Implement imrpot/export menu

--- data/menu.js
+++ data/menu.js
@@ -1,26 +1,65 @@
-var sidebarMenu, toggleMenu, createMenu, preferenceMenu;
+var sidebarMenu,
+    toggleMenu,
+    createMenu,
+    preferenceMenu,
+    importMenu,
+    exportMenu;
+var filePicker;
 var strings;
 const checkChar = '✔';
 window.onload = function() {
-  sidebarMenu =  document.getElementById('sidebar-menu');
+  sidebarMenu = document.getElementById('sidebar-menu');
   sidebarMenu.addEventListener('click', function() {
     self.port.emit('sidebar-menu');
   }, true);
-  toggleMenu =  document.getElementById('toggle-menu');
+  toggleMenu = document.getElementById('toggle-menu');
   toggleMenu.addEventListener('click', function() {
     self.port.emit('toggle-menu');
   }, true);
-  createMenu =  document.getElementById('create-menu');
+  createMenu = document.getElementById('create-menu');
   createMenu.addEventListener('click', function() {
     self.port.emit('create-menu');
   }, true);
-  preferenceMenu =  document.getElementById('preference-menu');
+  importMenu = document.getElementById('import-menu');
+  importMenu.addEventListener('click', function() {
+    document.getElementById("file-picker").click();
+  }, true);
+  filePicker = document.getElementById('file-picker');
+  filePicker.addEventListener('change', function(e) {
+    var i;
+    var files = e.target.files;
+    for (i = 0; i < files.length; i++) {
+      var file = files[i];
+      console.log('import ' + file.name + ' ' + file.type);
+      if (file.type == 'application/json') {
+        var reader = new FileReader();
+        reader.onload = function(event) {
+          try {
+            console.log(reader.result);
+            var data = JSON.parse(reader.result);
+            self.port.emit('import-menu', data);
+          } catch (e) {
+            console.log('failed to import:' + e);
+          }
+        };
+        reader.readAsText(file);
+      }
+    }
+  });
+
+  exportMenu = document.getElementById('export-menu');
+  exportMenu.addEventListener('click', function() {
+    self.port.emit('export-menu');
+  }, true);
+  preferenceMenu = document.getElementById('preference-menu');
   preferenceMenu.addEventListener('click', function() {
     self.port.emit('preference-menu');
   }, true);
 };
 
 var updateMenuLabel = function() {
+  importMenu.textContent = strings['stickyImportMenu.label'];
+  exportMenu.textContent = strings['stickyExportMenu.label'];
   sidebarMenu.textContent = (sidebarMenu.enabled ? checkChar : '') +
     strings['show_sticky_list.label'];
   toggleMenu.textContent = (toggleMenu.enabled ? checkChar : '') +
@@ -30,6 +69,21 @@ var updateMenuLabel = function() {
   preferenceMenu.textContent = (preferenceMenu.enabled ? checkChar : '') +
     strings['preferenceMenu.label'];
 }
+
+var downloadAsFile = function(fileName, content) {
+  console.log(fileName);
+  console.log(content);
+  var blob = new Blob([content]);
+  var url = window.URL;
+  var blobURL = url.createObjectURL(blob);
+
+  var a = document.createElement('a');
+  a.download = fileName;
+  a.href = blobURL;
+  var event = document.createEvent( "MouseEvents" );
+  event.initEvent("click", false, true);
+  a.dispatchEvent(event);
+};
 
 self.port.on('strings', function(_strings) {
   strings = _strings;
@@ -44,4 +98,9 @@ self.port.on('sidebar-menu', function(enabled) {
 self.port.on('toggle-menu', function(enabled) {
   toggleMenu.enabled = enabled;
   updateMenuLabel();
+});
+
+self.port.on('export', function(stickies) {
+  console.log(stickies);
+  downloadAsFile('stickynotes.json', JSON.stringify(stickies));
 });

2b312ec5fb0e4f0bb92f63c01cc50e62d798838d
Hiroki Kumamoto <kumabook@live.jp>
2015-06-13 13:25:36 +0900

    Add uuid generate helper function

--- test/test-DBHelper.js
+++ test/test-DBHelper.js
@@ -22,4 +22,22 @@ exports['test stickynotes.DBHelper.migrate'] = function(assert) {
   teardown();
 };
 
+exports['test stickynotes.DBHelper.uuid'] = function(assert) {
+  var uuid1 = stickynotes.DBHelper.uuid();
+  var uuid2 = stickynotes.DBHelper.uuid();
+  assert.equal(36, uuid1.length);
+  assert.notEqual(uuid1, uuid2);
+};
+
+exports['test stickynotes.DBHelper.getDateStr'] = function(assert) {
+  var date = new Date(1985, 3, 7, 0, 0, 0, 0);
+  var str = stickynotes.DBHelper.getDateStr(date);
+  assert.equal('1985-04-07 00:00:00', str);
+};
+
+exports['test stickynotes.DBHelper.getCurrentDateStr'] = function(assert) {
+  var str = stickynotes.DBHelper.getCurrentDateStr();
+  assert.notEqual(null, str);
+};
+
 require("sdk/test").run(exports);

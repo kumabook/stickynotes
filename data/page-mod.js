2be5b13dc53a19413beb3cf6bc9d04d544562619
Hiroki Kumamoto <kumabook@live.jp>
2016-05-14 23:16:03 +0900

    Improve logger of page-mod

--- data/page-mod.js
+++ data/page-mod.js
@@ -1,10 +1,27 @@
+var LEVEL = {
+    TRACE: 0,
+    DEBUG: 1,
+     INFO: 2,
+     WARN: 3,
+    ERROR: 4,
+    FATAL: 5
+};
+var level = LEVEL.FATAL;
 var logger = {
-  fatal: function(msg) {},
-  error: function(msg) {},
-  warn:  function(msg) {},
-  info:  function(msg) {},
-  debug: function(msg) {},
-  trace: function(msg) {}
+  LEVEL: LEVEL,
+  setLevel: function(newLevel) {
+    if (LEVEL[newLevel] !== undefined) {
+      level = LEVEL[newLevel];
+    } else {
+      level = newLevel;
+    }
+  },
+  fatal: function(msg) { if (level <= LEVEL.FATAL) console.error(msg); },
+  error: function(msg) { if (level <= LEVEL.ERROR) console.error(msg); },
+  warn:  function(msg) { if (level <= LEVEL.WARN)  console.warn(msg);  },
+  info:  function(msg) { if (level <= LEVEL.INFO)  console.info(msg);  },
+  debug: function(msg) { if (level <= LEVEL.DEBUG) console.debug(msg); },
+  trace: function(msg) { if (level <= LEVEL.TRACE) console.log(msg);   }
 };
 
 stickynotes.MARKER_ID = 'stickynotes-marker';

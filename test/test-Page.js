e232b8e38dc94c21fe70ce817986dd5f0518770a
Hiroki Kumamoto <kumabook@live.jp>
2016-05-21 22:16:56 +0900

    Rewrite Page test

--- test/test-Page.js
+++ test/test-Page.js
@@ -1,83 +1,70 @@
-var stickynotes = require('../lib/stickynotes');
-var setup = function() {
-  stickynotes.DBHelper.dropTables();
-  stickynotes.DBHelper.createTables();
-  stickynotes.DBHelper.migrate();
+const stickynotes = require('../lib/stickynotes');
+const TestHelper  = require('./TestHelper');
+
+const params  = { id: 100, url: 'http://test.jp' , title: 'test page'};
+const params2 = { id: 101, url: 'http://test1.jp', title: 'test1 page'};
+
+exports['test stickynotes.Page.create'] = function(assert, done) {
+  TestHelper.runDBTest(assert, done, function() {
+    return stickynotes.Page.create(params).then((page) => {
+      assert.ok(page != null);
+      return stickynotes.Page.create(params);
+    }).then(() => {
+      assert.fail('Cannot create page that has duplicated id');
+    }, () => {
+      assert.ok('Cannot create page that has duplicated id');
+    });
+  });
 };
 
-var teardown = function() {
-  stickynotes.DBHelper.dropTables();
+exports['test stickynotes.Page.fetchById()'] = function(assert, done) {
+  TestHelper.runDBTest(assert, done, function() {
+    return stickynotes.Page.create(params).then(() => {
+      return stickynotes.Page.fetchById(params.id);
+    }).then((page) => {
+      assert.equal(100, page.id);
+      assert.equal('http://test.jp', page.url);
+      assert.equal('test page' , page.title);
+    });
+  });
 };
 
-exports['test stickynotes.Page.create'] = function(assert) {
-  setup();
-  var page = stickynotes.Page.create({
-    id: 100,
-    url: 'http://test.jp',
-    title: 'test page'});
-  assert.ok(page != null);
-  assert.throws(
-    function() {
-      stickynotes.Page.create({
-        id: 100,
-        url: 'http://test.jp',
-        title: 'test page'
+exports['test stickynotes.Page.fetchByUrl()'] = function(assert, done) {
+  TestHelper.runDBTest(assert, done, function() {
+    const promises = [stickynotes.Page.create(params),
+                      stickynotes.Page.create(params2)];
+    return Promise.all(promises)
+      .then(() => stickynotes.Page.fetchByUrl('http://test.jp'))
+      .then((page) => {
+        assert.equal(100, page.id);
+        assert.equal('http://test.jp', page.url);
+        assert.equal('test page' , page.title);
+        return stickynotes.Page.fetchByUrl('http://nourl.jp');
+      }).then((nopage) => {
+        assert.equal(null, nopage, 'return null unexist url');
       });
-    },
-    stickynotes.DBHelper.DBAccessError);
+  });
 };
 
-exports['test stickynotes.Page.fetchById()'] = function(assert) {
-  setup();
-  stickynotes.Page.create(
-    { id: 100, url: 'http://test.jp', title: 'test page'});
-  var page = stickynotes.Page.fetchById(100);
-  assert.equal(100, page.id);
-  assert.equal('http://test.jp', page.url);
-  assert.equal('test page' , page.title);
-};
-
-exports['test stickynotes.Page.fetchByUrl()'] = function(assert) {
-  setup();
-  stickynotes.Page.create(
-    { id: 100, url: 'http://test.jp', title: 'test page'});
-  stickynotes.Page.create(
-    { id: 101, url: 'http://test1.jp', title: 'test1 page'});
-
-  var page = stickynotes.Page.fetchByUrl('http://test.jp');
-  assert.equal(100, page.id);
-  assert.equal('http://test.jp', page.url);
-  assert.equal('test page' , page.title);
-  var nopage = stickynotes.Page.fetchByUrl('http://nourl.jp');
-  assert.equal(null, nopage, 'return null unexist url');
-};
-
-exports['test stickynotes.Page.fetchAll()'] = function(assert) {
-  setup();
-  var pages = stickynotes.Page.fetchAll();
-  assert.equal(0, pages.length);
-
-  stickynotes.Page.create({ id: 100, url: 'http://test.jp', title: 'test page'});
-  pages = stickynotes.Page.fetchAll();
-  assert.equal(1, pages.length);
-  stickynotes.Page.create({ id: 101, url: 'http://test1.jp', title: 'test1 page'});
-
-  pages = stickynotes.Page.fetchAll();
-  assert.equal(2, pages.length);
-
-  assert.equal(100, pages[0].id);
-  assert.equal('http://test.jp', pages[0].url);
-  assert.equal('test page' , pages[0].title);
-
-  assert.throws(
-    function() {
-      stickynotes.Page.create({
-        id: 101,
-        url: 'http://test1.jp',
-        title: 'test page'
-      });
-    },
-    stickynotes.DBHelper.DBAccessError);
+exports['test stickynotes.Page.fetchAll()'] = function(assert, done) {
+  TestHelper.runDBTest(assert, done, function() {
+    return stickynotes.Page.fetchAll().then((pages) => {
+      assert.equal(0, pages.length);
+      return stickynotes.Page.create(params);
+    }).then(() => {
+      return stickynotes.Page.fetchAll();
+    }).then((pages) => {
+      assert.equal(1, pages.length);
+      return stickynotes.Page.create(params2);
+    }).then(() => {
+      return stickynotes.Page.fetchAll();
+    }).then((pages) => {
+      assert.equal(2, pages.length);
+      assert.equal(100, pages[0].id);
+      assert.equal('http://test.jp', pages[0].url);
+      assert.equal('test page' , pages[0].title);
+    });
+  });
 };
 
 require("sdk/test").run(exports);

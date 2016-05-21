e1c26fba27405db67329d4a0b1da7574dc6c7cae
Hiroki Kumamoto <kumabook@live.jp>
2016-05-21 22:17:26 +0900

    Rewrite DBHelper

--- test/test-DBHelper.js
+++ test/test-DBHelper.js
@@ -2,24 +2,30 @@ const stickynotes = require('../lib/stickynotes');
 const TestHelper  = require('./TestHelper');
 
 exports['test get database connection'] = function(assert) {
-  return  stickynotes.DBHelper.connection().then((c) => {
+  return stickynotes.DBHelper.connection().then((c) => {
     assert.ok(c != null, 'get dbconnection');
     return c.close();
   });
 };
 
-exports['test stickynotes.DBHelper.migrate'] = function(assert) {
-  TestHelper.runDBTest(assert, function(c) {
-    return stickynotes.DBHelper.getVersion(c).then((version) => {
-      assert.equal(0, version);
-      return stickynotes.DBHelper.migrate(c);
-    }).then(() => {
-      return stickynotes.DBHelper.getVersion(c);
-    }).then((version) => {
-      assert.equal(1, version);
-      return true;
+exports['test stickynotes.DBHelper.migrate'] = function(assert, done) {
+  TestHelper.runDBMigrationTest(assert, done, function() {
+    return stickynotes.DBHelper.connection().then((c) => {
+      return stickynotes.DBHelper.getVersion(c).then((version) => {
+        assert.equal(0, version);
+        return stickynotes.DBHelper.migrate(c);
+      }).then(() => {
+        return stickynotes.DBHelper.getVersion(c);
+      }).then((version) => {
+        assert.equal(1, version);
+        return true;
+      }).then(() => {
+        c.close();
+      }, () => {
+        c.close();
+      });
     });
-  });
+  }, true);
 };
 
 exports['test stickynotes.DBHelper.uuid'] = function(assert) {

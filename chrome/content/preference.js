8d3f72290182d29c0468099e80db773800e70d57
Hiroki Kumamoto <kumabook@live.jp>
2016-11-06 23:47:29 +0900

    [#71] Add colors preference pane

--- chrome/content/preference.js
+++ chrome/content/preference.js
@@ -4,6 +4,11 @@ win.shortcutsTree =
 win.shortcutsTreeChildren =
   document.getElementById('stickynotes-shortcuts-treechildren');
 
+win.colorsTree =
+  document.getElementById('stickynotes-colors-tree');
+win.colorsTreeChildren =
+  document.getElementById('stickynotes-colors-treechildren');
+
 stickynotes.reloadShortcuts = function(shortcuts) {
   shortcuts.forEach(function(shortcut) {
     var elem = this.document.getElementById(shortcut.name);
@@ -59,4 +64,42 @@ win.addEventListener('keydown', function(e) {
 
 if (win.shortcuts) {
   stickynotes.createShortcuts(win.shortcuts);
+}
+
+stickynotes.createColors = function(colors) {
+  colors.forEach(function(color) {
+    var treeitem = win.document.createElement('treeitem');
+    var treerow = win.document.createElement('treerow');
+    var idTreecell = win.document.createElement('treecell');
+    idTreecell.setAttribute('label', color.id);
+    idTreecell.setAttribute('value', color.id);
+    idTreecell.setAttribute('editable', false);
+    var backgroundTreecell = win.document.createElement('treecell');
+    backgroundTreecell.setAttribute('id', color.id + '-background');
+    backgroundTreecell.setAttribute('editable', true);
+
+    var fontTreecell = win.document.createElement('treecell');
+    fontTreecell.setAttribute('id', color.id + '-font');
+    fontTreecell.setAttribute('editable', true);
+
+    treeitem.appendChild(treerow);
+    treerow.appendChild(idTreecell);
+    treerow.appendChild(backgroundTreecell);
+    treerow.appendChild(fontTreecell);
+    win.colorsTreeChildren.appendChild(treeitem);
+  });
+  stickynotes.reloadColors(colors);
+};
+
+stickynotes.reloadColors = function(colors) {
+  colors.forEach(function(color) {
+    var background = this.document.getElementById(color.id + '-background');
+    background.setAttribute('label', color.background);
+    var font = this.document.getElementById(color.id + '-font');
+    font.setAttribute('label', color.font);
+  });
+};
+
+if (win.colors) {
+  stickynotes.createColors(win.colors);
 }

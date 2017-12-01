d505112e5f7f46e376b7bde49358d407c952d7db
Hiroki Kumamoto <kumabook@live.jp>
2017-12-01 22:54:31 +0900

    Add setting files for unittest

---
+++ test/setup.js
@@ -0,0 +1,35 @@
+const { JSDOM }   = require('jsdom');
+const enzyme      = require('enzyme');
+const Adapter     = require('enzyme-adapter-react-16');
+const logger      = require('kiroku');
+const browser     = require('./browser_mock');
+const indexedDB   = require('fake-indexeddb');
+const IDBKeyRange = require('fake-indexeddb/lib/FDBKeyRange');
+
+const body = '<div id="container" />';
+const jsdom = new JSDOM(`<!doctype html><html><body>${body}</body></html>`);
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .map(prop => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+global.indexedDB = indexedDB;
+global.IDBKeyRange = IDBKeyRange;
+
+
+global.browser = browser;
+global.chrome = null;
+
+copyProps(window, global);
+
+enzyme.configure({ adapter: new Adapter() });
+logger.setLevel('FATAL');

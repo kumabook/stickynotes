406d42a3e8e7872da08996afa5fd4ad8cdbb507f
Hiroki Kumamoto <kumabook@live.jp>
2017-12-06 15:12:25 +0900

    Add index.js. It runs webpack and web-ext run in same process. It is used from `npm run start`

---
+++ index.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const webExt  = require('web-ext').default;
+const config  = require('./webpack.config');
+
+const compiler = webpack(config);
+const watching = compiler.watch({}, (err) => {
+  /* eslint-disable no-console */
+  if (err) {
+    console.log('\nFailed to webpack build');
+  } else {
+    console.log('\nweback built');
+  }
+});
+
+webExt.cmd.run({
+  firefox:        process.env.FIREFOX_BINARY,
+  sourceDir:      process.cwd(),
+  ignoreFiles:    process.env.IGNORE_FILES.split(' ').concat('**/*~'),
+  browserConsole: true,
+}, {
+  shouldExitProgram: false,
+}).then(runner => runner.registerCleanup(() => watching.close()));
